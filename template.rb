7e92a959253dd77fb1604a7409dc1561f1aa8ca5
kmb <kmb@comp-mebel1.(none)>
2009-03-18 15:02:25 +0300

    add git prawn and nifty

--- template.rb
+++ template.rb
@@ -1,2 +1,23 @@
 # sample template
+if yes?("Use Git?")
+  git :init
+  run "touch tmp/.gitignore log/.gitignore vendor/.gitignore"
+  file '.gitignore', <<-END
+  .DS_Store
+  log/*.log
+  tmp/**/*
+  config/database.yml
+  db/*.sqlite3
+  END
+  git :add=>".", :commit=>"Initial commit"
+end
 
+if yes?("Nifty layout?")
+  generate :nifty_layout
+end
+if yes?("Use Prawn (pdf)?")
+  gem "prawn"
+  plugin "prawnto", :git => "git://github.com/thorny-sun/prawnto.git"
+end
+  
+rake "gems:install"
